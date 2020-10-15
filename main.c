f738f0d727cc7473dac3a50d2cc52f4c2a660462
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-15 15:46:50 +0900

    실습2
    12345

--- main.c
+++ main.c
@@ -7,13 +7,14 @@ int main(int argc, char *argv[])
  {
  	
 	int i; 
+	
 	for (i=0; i<5; i++) 	
  	{
- 		int temp = 1;
+ 		static int temp = 1;
  		printf("temp = %d\n", temp);
  		temp++;
 	}
- 	return 0;
- 
+ 	
+ return 0;
 }
 
