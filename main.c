f9e16d36251fc5a2ec4b26f67e35a33e01b214e8
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-15 15:35:02 +0900

    실습1

--- main.c
+++ main.c
@@ -5,16 +5,14 @@
 
 int main(int argc, char *argv[])
  {
- 	int flag = 1;
- 	while(flag != 0)
- 	
+ 	int y;
+	 int flag = 1;
+ 	while(flag !=0)
  	{
- 		int y;
  		y=3;
  		flag = 0;
-	}
-	 
-	 y=4;
-	
-	
+	 }
+ 	
+ 
 }
+
